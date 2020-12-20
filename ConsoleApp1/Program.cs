deb1fa85b6d7650a78d80c6b90af7feb64d14ef8
serkan.yilmaz1 <serkan.yilmaz1@enerjisa.com>
2020-12-20 22:06:53 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            //Unary Call
+            //var response = await client.SayHelloAsync(new HelloRequest
+            //{
+            //    Name = "gRPC Demo"
+            //});
+
+            //Console.WriteLine("From Server: " + response.Message);
+
+            //Stream call
+            var call = client.SayHelloStream(new HelloRequest
+            {
+                Name = "gRPC Demo Streaming"
+            });
+
+            await foreach (var item in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine("From server: " + item.Message);
+            }
+        }
+    }
+}
